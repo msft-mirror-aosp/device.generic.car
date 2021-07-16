c8823eb570a052da4e40ddccd6331b8b0d63a209
Yu Shan <shanyu@google.com>
2021-07-16 15:53:51 -0700

    Move emulatedUserHal to default VHAL.
    Move emualtedUserHal, fake OBD2 frame logic from emulatedVHAL to
    default VHAL. Those logic might be required for VHAL clients to work
    on non-emulator targets.
    
    Test: Verify user hal logic works on defualt VHAL.
    Bug: 193831021
    Change-Id: I23caae945d279407f8159f9f8c0b7b010262da9a

--- emulator/vhal_v2_0/EmulatedVehicleHal.h
+++ emulator/vhal_v2_0/EmulatedVehicleHal.h
@@ -17,20 +17,9 @@
 #ifndef android_hardware_automotive_vehicle_V2_0_impl_EmulatedVehicleHal_H_
 #define android_hardware_automotive_vehicle_V2_0_impl_EmulatedVehicleHal_H_
 
-#include <sys/socket.h>
-#include <map>
-#include <memory>
-#include <thread>
-#include <unordered_set>
-
-#include <utils/SystemClock.h>
-
 #include <vhal_v2_0/DefaultVehicleHal.h>
-#include <vhal_v2_0/RecurrentTimer.h>
-#include <vhal_v2_0/VehicleHal.h>
+#include <vhal_v2_0/VehicleHalClient.h>
 #include <vhal_v2_0/VehiclePropertyStore.h>
-
-#include "EmulatedUserHal.h"
 
 namespace android {
 namespace hardware {
@@ -43,32 +32,8 @@ namespace impl {
 /** Implementation of VehicleHal that connected to emulator instead of real vehicle network. */
 class EmulatedVehicleHal : public DefaultVehicleHal {
   public:
-    EmulatedVehicleHal(VehiclePropertyStore* propStore, VehicleHalClient* client,
-                       EmulatedUserHal* emulatedUserHal = nullptr);
+    EmulatedVehicleHal(VehiclePropertyStore* propStore, VehicleHalClient* client);
     ~EmulatedVehicleHal() = default;
-
-    //  Methods from VehicleHal
-    void onCreate() override;
-    VehiclePropValuePtr get(const VehiclePropValue& requestedPropValue,
-                            StatusCode* outStatus) override;
-    StatusCode set(const VehiclePropValue& propValue) override;
-    void getAllPropertiesOverride();
-
-  private:
-    StatusCode handleGenerateFakeDataRequest(const VehiclePropValue& request);
-
-    void initObd2LiveFrame(const VehiclePropConfig& propConfig);
-    void initObd2FreezeFrame(const VehiclePropConfig& propConfig);
-    StatusCode fillObd2FreezeFrame(const VehiclePropValue& requestedPropValue,
-                                   VehiclePropValue* outValue);
-    StatusCode fillObd2DtcInfo(VehiclePropValue* outValue);
-    StatusCode clearObd2FreezeFrames(const VehiclePropValue& propValue);
-
-    /* Private members */
-    std::unordered_set<int32_t> mHvacPowerProps;
-    bool mInitVhalValueOverride;
-    std::vector<VehiclePropValue> mVehiclePropertiesOverride;
-    EmulatedUserHal* mEmulatedUserHal;
 };
 
 }  // namespace impl
