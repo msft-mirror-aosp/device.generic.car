7e60365a0c43a9261a2a81cbb0d5f08eb83f12f3
Calvin Huang <calhuang@google.com>
2023-12-07 14:39:00 -0800

    Impl overlay for skins
    Bug: 308472512
    Test: launch emulator w/ skins
    Change-Id: I40ab80b5120dce43e093a1be6ab6383056c77cad

--- emulator/skins/overlays/car_emu_skin_overlays.mk
+++ emulator/skins/overlays/car_emu_skin_overlays.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2023 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,23 +12,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-parts {
-  device {
-    display {
-      width 1408
-      height 792
-    }
-  }
-}
-layouts {
-  landscape {
-    width 1500
-    height 837
-    part1 {
-      name device
-      x 50
-      y 30
-    }
-  }
-}
+
+
+# Overlays for skins
+PRODUCT_PACKAGES += \
+    CarEmulationAutomotiveUltrawideOverlay \
+    CarSystemUIEmulationutomotiveLargePortraitOverlay \
+    CarSystemUIEmulationutomotiveUltrawideOverlay
