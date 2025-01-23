634edf96a9e44c6e37d6406d7fd4003c32cda7da
Calvin Huang <calhuang@google.com>
2025-01-23 15:24:08 -0800

    Add sdk_car_dewd_x86_64 target
    Bug: 391946744
    Test: lunch sdk_car_dewd_x86_64-trunk_staging-userdebug
    Flag: com.android.systemui.car.scalable_ui
    Change-Id: I430feedd045a85e548816c6d70386fbc76c23084

---
+++ sdk_car_dewd_x86_64.mk
@@ -0,0 +1,43 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Car UI Emulator Target with Declarative windowing definition
+
+# Exclude GAS Car Launcher
+DO_NOT_INCLUDE_GAS_CAR_LAUNCHER := true
+
+# # Exclude Car UI Reference Design
+DO_NOT_INCLUDE_CAR_UI_REFERENCE_DESIGN := true
+
+# Exclude Car Visual Overlay
+DISABLE_CAR_PRODUCT_VISUAL_OVERLAY := true
+
+# Copy additional files
+PRODUCT_COPY_FILES += \
+    packages/services/Car/car_product/dewd/car_dewd_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/car_dewd_hardware.xml \
+    packages/services/Car/car_product/dewd/config.ini:config.ini
+
+$(call inherit-product, packages/services/Car/car_product/dewd/car_dewd_common.mk)
+$(call inherit-product, device/generic/car/sdk_car_x86_64.mk)
+
+# TODO(b/303863968): Set it to true after cleaning up the system partition
+# changes from this makefile
+PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := false
+
+PRODUCT_NAME := sdk_car_dewd_x86_64
+PRODUCT_MODEL := Declarative windowing definition on x86_64 emulator
+PRODUCT_CHARACTERISTICS := automotive
+PRODUCT_SDK_ADDON_NAME := sdk_car_dewd_x86_64
+
