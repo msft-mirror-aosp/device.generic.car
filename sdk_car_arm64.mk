fa0692e98368c0ceb364f834381d59ec3130698b
Sihu Song <sihusong@google.com>
2025-04-02 13:04:01 +0900

    sdk_car targets exclude CarProvision by adding AOSP_CAR_EMULATOR flag to true
    Bug: 407859763
    Test: `get_build_var PRODUCT_PACKAGES | grep CarProvision` shows nothing for sdk_car targets
    Change-Id: I8d52283e4884ba1d307ceb79dc6a1e640ea36fed

--- sdk_car_arm64.mk
+++ sdk_car_arm64.mk
@@ -40,7 +40,9 @@ PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict
 
 #
 # All components inherited here go to system_ext image
+# But CarProvision must be excluded
 #
+AOSP_CAR_EMULATOR := true
 $(call inherit-product, packages/services/Car/car_product/build/car_system_ext.mk)
 
 #
