d0d25090f42426ddc8284a091a9fe0021c094886
Yu Shan <shanyu@google.com>
2025-05-29 11:50:21 -0700

    Update emulator VHAL name and manifest to V4.
    Emulator VHAL inherits from default VHAL which implements V4 interface.
    
    Test: Presubmit
    Bug: 421205697
    Flag: EXEMPT ref HAL change.
    Change-Id: I8eff704474cc13c5c934a064a6ba54a6530afab1

--- emulator/car_emulator_vendor.mk
+++ emulator/car_emulator_vendor.mk
@@ -52,7 +52,7 @@ PRODUCT_COPY_FILES += \
 
 # Auto modules
 PRODUCT_PACKAGES += \
-    android.hardware.automotive.vehicle@V3-emulator-service \
+    android.hardware.automotive.vehicle@V4-emulator-service \
     android.hardware.broadcastradio-service.default \
     android.hardware.audio.service-caremu \
     android.hardware.automotive.remoteaccess@V2-default-service \
