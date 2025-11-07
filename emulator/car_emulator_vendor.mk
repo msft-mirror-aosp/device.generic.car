041ec1943aac6b4dcfdf2f3cab624af496d2afb4
Jusik Chung <jschung@google.com>
2025-11-07 04:20:01 +0000

    Enable usb host feature
    USB host feature is required for AOA.
    
    Bug: 458538461
    Test: adb shell pm list features, adb shell pm list packages | grep
    projection
    Flag: EXEMPT BUGFIX
    
    Change-Id: I8ae4886e70712dd8cc1d450f264d4bc01431b178

--- emulator/car_emulator_vendor.mk
+++ emulator/car_emulator_vendor.mk
@@ -99,6 +99,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.heading.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.heading.xml \
 
+ifeq (true,$(ENABLE_EMULATOR_USB_HOST))
+# USB Host mode
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+endif
+
 # Additional selinux policy
 BOARD_SEPOLICY_DIRS += device/generic/car/common/sepolicy
 
